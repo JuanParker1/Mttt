401dce418cc068501180720b56af9bb9415578d9
StormWixGOD <103872199+StormWixGOD@users.noreply.github.com>
2022-04-28 23:54:52 -0500

    Update and rename modules/iban.php to módulos/iban.php

--- módulos/iban.php
+++ módulos/iban.php
@@ -89,7 +89,7 @@ if(strpos($message, "/iban ") === 0){
 <ins>Time:</ins> | <code>$timetaken</code><code>s</code>
 <b>━━━━━━━━━━</b>
 <b>🌟Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>Bot By: <a href='t.me/yhvga'>yhvga</a></b>" 🤴,
+<b>Bot By: <a href='t.me/yhvga'>yhvga</a></b>",
                 'parse_mode'=>'html',
                 'disable_web_page_preview'=>'true'
                 
