cd7f210999990c3bd0c3820514330487279003e3
StormWixGOD <103872199+StormWixGOD@users.noreply.github.com>
2022-04-29 17:57:03 -0500

    Update me.php

--- modules/me.php
+++ modules/me.php
@@ -35,13 +35,13 @@ if(strpos($message, "/me") === 0 || strpos($message, "!me") === 0){
     }else{
         $messageidtoedit1 =   bot('sendmessage',[
           'chat_id'=>$chat_id,
-          'text'=>"↱  <b>User Info</b>
+          'text'=>"≡ <b>User Info</b>
 - <ins>User ID:</ins> <code>$userId</code>
 - <ins>Full Name:</ins> ".htmlspecialchars($firstname.$lastname)."
 - <ins>User Name:</ins> @$username
 - <ins>User Type:</ins> <b>Free User</b>
-<><><>><><><><><><><><><><><><><><><><><><><><>
-<b>$date1 | $time</b>",
+━━━━━━━━━━━━━=
+<b>$date1 $time</b>",
           'parse_mode'=>'html',
           'reply_to_message_id'=> $message_id,
           'reply_markup'=>json_encode(['inline_keyboard'=>[
@@ -57,13 +57,14 @@ if($data == "checkerstats"){
         bot('editMessageText',[
           'chat_id'=>$callbackchatid,
           'message_id'=>$callbackmessageid,
-          'text'=>"↱ <b>User Stats</b>
+          'text'=>"≡ <b>User Stats</b>
+          
 - <ins>Total Cards Checked:</ins> ".$uStats['total_checked']."
 - <ins>Total CVV Cards:</ins> ".$uStats['total_cvv']."
 - <ins>Total CCN Cards:</ins> ".$uStats['total_ccn']."
-----------------------------         
+          
 ≡ <b>Global Checker Stats</b>
-----------------------------         
+
 - <ins>Total Cards Checked:</ins> ".$gStats['total_checked']."
 - <ins>Total CVV Cards:</ins> ".$gStats['total_cvv']."
 - <ins>Total CCN Cards:</ins> ".$gStats['total_ccn']."",
@@ -81,13 +82,13 @@ if($data == "backme"){
   bot('editMessageText',[
     'chat_id'=>$callbackchatid,
     'message_id'=>$callbackmessageid,
-    'text'=>"↱  <b>User Info</b>
+    'text'=>"≡ <b>User Info</b>
 - <ins>User ID:</ins> <code>$callbackuserid</code>
 - <ins>Full Name:</ins> ".htmlspecialchars($callbackfname.$callbacklname)."
 - <ins>User Name:</ins> @$callbackusername
 - <ins>User Type:</ins> <b>Free User</b>
-<><><>><><><><><><><><><><><><><><><><><><><><>
-<b>$date1 | $time</b>",
+━━━━━━━━━━━━━=
+<b>$date1 $time</b>",
       'parse_mode'=>'html',
       'reply_to_message_id'=> $message_id,
       'reply_markup'=>json_encode(['inline_keyboard'=>[
