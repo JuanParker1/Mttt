fc9596f1ab0f9905dcfa0cc7c8245510cdb015f2
StormWixGOD <103872199+StormWixGOD@users.noreply.github.com>
2022-05-01 13:53:04 -0500

    Update binlookup.php

--- modules/binlookup.php
+++ modules/binlookup.php
@@ -49,10 +49,10 @@ if(strpos($message, "/bin ") === 0 || strpos($message, "!bin ") === 0){
 
             ###CHECKER PART###  
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'https://bin-check-dr4g.herokuapp.com/api/'.$bin.'');
+            curl_setopt($ch, CURLOPT_URL, 'https://binsu-api.vercel.app/api/'.$bin.'');
             curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Host: bin-check-dr4g.herokuapp.com/api/',
+            'Host: binsu-api.vercel.app/api/',
             'Cookie: _ga=GA1.2.549903363.1545240628; _gid=GA1.2.82939664.1545240628',
             'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'));
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
