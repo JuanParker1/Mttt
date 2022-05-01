c6a45bea1bf627d109d93b438e17fe09182fde7a
StormWixGOD <103872199+StormWixGOD@users.noreply.github.com>
2022-05-01 17:26:51 -0500

    Update binlookup.php

--- modules/binlookup.php
+++ modules/binlookup.php
@@ -48,25 +48,34 @@ if(strpos($message, "/bin ") === 0 || strpos($message, "!bin ") === 0){
         
 
             ###CHECKER PART###  
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'https://binsu-api.vercel.app/api/'.$bin.'');
-            curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Host: binsu-api.vercel.app/api/',
-            'Cookie: _ga=GA1.2.549903363.1545240628; _gid=GA1.2.82939664.1545240628',
-            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'));
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, '');
-            $fim = curl_exec($ch);
-            $data = json_decode($result, true);
-            $bank = $data['data']['bank'];
-            $country = $data['data']['country'];
-            $brand = $data['data']['vendor'];
-            $level = $data['data']['level'];
-            $type = $data['data']['type'];
-            $flag = $data['data']['countryInfo']['emoji'];
-            $result1 = $data['result'];
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+    CURLOPT_URL => "https://binsu-api.vercel.app/api/".$bin,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+    "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
+    "sec-fetch-dest: document",
+    "sec-fetch-site: none",
+    "user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+   ],
+   ]);
+
+ $result = curl_exec($curl);
+ curl_close($curl);
+ $data = json_decode($result, true);
+ $bank = $data['data']['bank'];
+ $country = $data['data']['country'];
+ $brand = $data['data']['vendor'];
+ $level = $data['data']['level'];
+ $type = $data['data']['type'];
+$flag = $data['data']['countryInfo']['emoji'];
 
             /////////////////////==========[Unavailable if empty]==========////////////////
             
